b7bb75e6402fdd220fad09050909c4448e44b243
Teddy Terezis <tterezis@chicagobooth.edu>
2023-04-17 22:55:33 +0000

    Latest Commit

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -15,7 +15,18 @@ class CurrenciesController < ApplicationController
 
     @array_of_symbols= @symbols_hash.keys
     @from_symbol = params.fetch("from_currency")
-    
+
       render({:template => "currency_templates/step_two.html.erb"})
+  end
+  def third_currency
+    @raw_data=open("https://api.exchangerate.host/symbols").read
+    @parsed_data = JSON.parse(@raw_data)
+    @symbols_hash = @parsed_data.fetch("symbols")
+
+    @array_of_symbols= @symbols_hash.keys
+    @from_symbol = params.fetch("from_currency")
+    @to_symbol = params.fetch("to_currency")
+
+      render({:template => "currency_templates/step_three.html.erb"})
   end
 end
