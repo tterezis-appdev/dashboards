1fd675762c1c5ce20c50e0b2e5c798d57e7e9014
Teddy Terezis <tterezis@chicagobooth.edu>
2023-04-18 01:56:40 +0000

    All done

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -19,13 +19,14 @@ class CurrenciesController < ApplicationController
       render({:template => "currency_templates/step_two.html.erb"})
   end
   def third_currency
-    @raw_data=open("https://api.exchangerate.host/symbols").read
-    @parsed_data = JSON.parse(@raw_data)
-    @symbols_hash = @parsed_data.fetch("symbols")
-
-    @array_of_symbols= @symbols_hash.keys
     @from_symbol = params.fetch("from_currency")
     @to_symbol = params.fetch("to_currency")
+    @raw_data=open("https://api.exchangerate.host/convert?from=#{@from_symbol}&to=#{@to_symbol}").read
+    @parsed_data = JSON.parse(@raw_data)
+    @result=@parsed_data.fetch("result")
+
+  
+    
 
       render({:template => "currency_templates/step_three.html.erb"})
   end
